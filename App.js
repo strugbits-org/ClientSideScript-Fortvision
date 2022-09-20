4cfba6ffa02a0814d57c9c5a1da27d869729b20e
team-strugbits <84453625+team-strugbits@users.noreply.github.com>
2022-09-20 15:14:42 +0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,7 @@
+        console.log("CLIENT SIDE SCRIPT RAN..");
+        const pageViewHandler = (eventData) => {
+            console.log('PAGE VIEW EVENT TRIGGERED!!', eventData)
+        }
+        const addToCartHandler = (eventData) => {
+            console.log('ADD TO CARD EVENT TRIGGERED!!', eventData)
+        }
